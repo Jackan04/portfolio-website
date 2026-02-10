bf740d89a56a630ea8f864d2eabc48db53146905
jackan04 <jacob.asker@icloud.com>
2026-02-10 21:16:32 +0100

    -added profile image to assets folder -removed CSS transitions on has-over class removed link to favicon from Vite in index.html

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,1 +1,13 @@
-export default function Home() {}
+import profileImage from "../assets/images/profile.jpeg";
+
+export default function Home() {
+  return (
+    <img
+      width={100}
+      height={100}
+      src={profileImage}
+      loading="lazy"
+      alt="Profile Image"
+    />
+  );
+}

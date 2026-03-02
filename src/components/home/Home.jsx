496bc6a4e44fb0aa8fdbfee868f20dfd4809146b
jackan04 <jacob.asker@icloud.com>
2026-03-02 06:39:21 +0100

    -adjusted the hero header text and added a ”Get in Touch” section. -replaced the <div> with a classname section to use a semantic section-element. -adjusted the section spacing on mobile devices

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,38 +6,48 @@ import Link from "../Link.jsx";
 export default function Home() {
   return (
     <>
-      <div className={styles.section}>
+      <section>
         <img
           src={profileImage}
           loading="lazy"
-          alt="Profile Image"
+          alt="Jacob Asker"
           className={styles.profileImage}
         />
-        <h2>
-          Informatics student with a passion for building and designing digital
-          things.
-        </h2>
-      </div>
-      <div className={styles.section}>
+        <h2>Full-Stack Developer and Informatics Student</h2>
+      </section>
+
+      <section>
         <h2>About Me</h2>
         <p>
-          I’m currently finishing my bacherlors degree in informatics, where I
-          spend most of my time exploring the intersection of code and user
-          experience. When I’m not at the keyboard, you’ll usually find me
-          hiking with friends, lifting at the gym, or behind a camera.
-        </p>
-        <p>
-          I’m always open to new opportunities. Feel free to{" "}
-          <Link className="inline-link" href="mailto:jacob.asker@icloud.com">
-            get in touch
+          I’m currently finishing my Bachelor’s in Informatics, where I bridge
+          the gap between technical implementation and user experience. I enjoy
+          turning ideas into practical, intuitive experiences and learning by
+          building real projects. You can explore my latest work on my
+          <Link className="inline-link" href="#/projects" target="_self">
+            {" "}
+            projects page
           </Link>
           .
         </p>
-      </div>
-      <div className={styles.section}>
+      </section>
+
+      <section>
+        <h2>Get in Touch</h2>
+        <p>
+          I’m looking for opportunities to apply my skills in a professional
+          environment. If you think I’d be a good fit,
+          <Link className="inline-link" href="mailto:jacob.asker@icloud.com">
+            {" "}
+            I'd love to hear from you
+          </Link>
+          .
+        </p>
+      </section>
+
+      <section>
         <h2>Technical Experience</h2>
         <ExperienceTable />
-      </div>
+      </section>
     </>
   );
 }

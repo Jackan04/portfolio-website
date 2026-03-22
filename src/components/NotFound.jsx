de4460e9e8c9756d2768a112f3489eca1793d75b
jackan04 <jacob.asker@icloud.com>
2026-03-22 10:23:15 +0100

    refactor: use <section> instead of react fragment for consistency

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <>
+    <section>
       <h2>404 - Page Not Found</h2>
       <p>
         The page you're looking for seems to have wandered off. Don't worry, it
@@ -11,6 +11,6 @@ export default function NotFound() {
       <NavLink to="/" className="button hover-opacity">
         Take Me Home
       </NavLink>
-    </>
+    </section>
   );
 }

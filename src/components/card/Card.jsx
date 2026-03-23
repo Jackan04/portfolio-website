7a62be155ceee7a493a68c90a42296ac84af0e49
jackan04 <jacob.asker@icloud.com>
2026-03-23 15:43:27 +0100

    add chevron-right icon and update card button with ghost class

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,15 @@
 import styles from "./Card.module.css";
 import CustomLink from "../CustomLink";
+import ChevronRightIcon from "../../assets/icons/chevron-right.svg?react";
 
 export default function Card({ title, desc, buttonLabel, url }) {
   return (
     <div className={styles.card}>
       <h3 className={styles.title}>{title}</h3>
       <p className="body-secondary">{desc}</p>
-      <CustomLink href={url} className="button outline small hover-accent">
+      <CustomLink href={url} className="button small ghost">
         {buttonLabel}
+        <ChevronRightIcon className="icon-small" />
       </CustomLink>
     </div>
   );

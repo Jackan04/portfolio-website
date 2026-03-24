411a5f154a3f21a090a833de5c4df8918a990cc8
jackan04 <jacob.asker@icloud.com>
2026-03-24 08:46:18 +0100

    increase space on cards, replace h3 with b

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,7 +6,7 @@ export default function Card({ title, desc, buttonLabel, url }) {
   return (
     <div className={styles.card}>
       <div className={styles.text}>
-        <h3 className={styles.title}>{title}</h3>
+        <b>{title}</b>
         <small>{desc}</small>
       </div>
       <CustomLink href={url} className="button small ghost">

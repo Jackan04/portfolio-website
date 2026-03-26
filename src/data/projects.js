4b44715766f4f24abb07929c3ba52f7404b369d8
jackan04 <jacob.asker@icloud.com>
2026-03-26 17:13:28 +0100

    feat: add featured boolean property

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,6 +6,7 @@ const projects = [
     url: "https://github.com/Jackan04/chat-app-client",
     title: "Chat App",
     preview: "Messaging app with a REST API and React client.",
+    featured: true,
   },
   {
     category: categories[0],
@@ -13,36 +14,43 @@ const projects = [
     title: "Blog Platform",
     preview:
       "Full-stack blogging platform with API, admin, and public clients.",
+    featured: false,
   },
+
   {
     category: categories[0],
     url: "https://github.com/Jackan04/file-uploader",
     title: "File Uploader",
     preview: "Full-stack Node.js app for uploading and managing files.",
+    featured: true,
   },
   {
     category: categories[0],
     url: "https://github.com/Jackan04/Ticky",
     title: "Task Manager",
     preview: "Simple personal task manager for lists and due dates.",
+    featured: false,
   },
   {
     category: categories[1],
     url: "https://github.com/Jackan04/MediaTracker?tab=readme-ov-file",
     title: "Media Tracker",
     preview: "Mobile watchlist app for movies and TV shows.",
+    featured: false,
   },
   {
     category: categories[1],
     url: "https://github.com/Jackan04/ShelfLife",
     title: "Shelf Life",
     preview: "Mobile app to track groceries and expiration dates.",
+    featured: false,
   },
   {
     category: categories[2],
     url: "https://www.linkedin.com/feed/update/urn:li:activity:7356214960147832832/?originTrackingId=eSg5eAa5mcuCMkl2pRHjqw%3D%3D",
     title: "Redesign of Self-Checkout",
     preview: "UX redesign concept to simplify self-checkout flows.",
+    featured: false,
   },
 ];
 

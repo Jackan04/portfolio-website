b11ddb2154d3ec82b4b09eab47c0e8b96905ca55
jackan04 <jacob.asker@icloud.com>
2026-03-26 15:29:11 +0100

    refactor: update Card layout
    wrap card in CustomLink
    remove button
    add ChevronIcon in cardHeader
    add hover-effect on card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,17 +2,14 @@ import styles from "./Card.module.css";
 import CustomLink from "../CustomLink";
 import ChevronRightIcon from "../../assets/icons/chevron-right.svg?react";
 
-export default function Card({ title, desc, buttonLabel, url }) {
+export default function Card({ title, desc, url }) {
   return (
-    <div className={styles.card}>
-      <div className={styles.text}>
+    <CustomLink href={url} className={styles.card}>
+      <div className={styles.header}>
         <h3>{title}</h3>
-        <p className="body-secondary">{desc}</p>
+        <ChevronRightIcon className={`icon-small ${styles.chevron}`} />
       </div>
-      <CustomLink href={url} className="button small ghost">
-        {buttonLabel}
-        <ChevronRightIcon className="icon-small" />
-      </CustomLink>
-    </div>
+      <p className={`body-secondary ${styles.text}`}>{desc}</p>
+    </CustomLink>
   );
 }

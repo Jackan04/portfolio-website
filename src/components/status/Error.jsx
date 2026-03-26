11f7dd1e9a749fbc9c16d296320f26e7ca900512
jackan04 <jacob.asker@icloud.com>
2026-03-26 14:06:23 +0100

    update card title

--- src/components/status/Error.jsx
+++ src/components/status/Error.jsx
@@ -3,7 +3,7 @@ import styles from "./Status.module.css";
 export default function Error({ message }) {
   return (
     <div className={`${styles.statusContainer} ${styles.error}`}>
-      <h3>Error</h3>
+      <h3>There was an error</h3>
       <p>{message}</p>
     </div>
   );

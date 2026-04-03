99eb14510a02073ee1c3bb6ff7513b324b790551
jackan04 <jacob.asker@icloud.com>
2026-04-03 09:00:48 +0200

    implement BlogCard component

---
+++ src/components/card/BlogCard.jsx
@@ -0,0 +1,22 @@
+import styles from "./Card.module.css";
+import CustomLink from "../CustomLink";
+import ChevronRightIcon from "../../assets/icons/chevron-right.svg?react";
+
+export default function BlogCard({ title, summary, date, url }) {
+  const formatted = new Date(date).toLocaleDateString("en-SE", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+
+  return (
+    <CustomLink href={url} className={`hover-bg ${styles.card}`}>
+      <div className="hstack justify-between">
+        <h3>{title}</h3>
+        <ChevronRightIcon className={`icon-small ${styles.chevron}`} />
+      </div>
+      {summary && <small>{summary}</small>}
+      <small className={`text-secondary ${styles.date}`}>{formatted}</small>
+    </CustomLink>
+  );
+}

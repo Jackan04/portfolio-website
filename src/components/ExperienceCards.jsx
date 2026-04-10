4648c6e9ca955426fde08c1e74805d140f941263
jackan04 <jacob.asker@icloud.com>
2026-04-10 16:48:00 +0200

    feat: replace anchor tag with CustomLink component

--- src/components/ExperienceCards.jsx
+++ src/components/ExperienceCards.jsx
@@ -1,12 +1,13 @@
 import experiences from "../data/experiences.js";
 import Label from "./Label.jsx";
+import CustomLink from "./CustomLink.jsx";
 
 export default function ExperienceCards() {
   return (
     <div className="hstack">
       {experiences.map((item) => (
         <Label key={item.name}>
-          <a href={item.url}>{item.name}</a>
+          <CustomLink href={item.url}>{item.name}</CustomLink>
         </Label>
       ))}
     </div>

125311fe1edb3640845ae979688d807b3bdfe15b
jackan04 <jacob.asker@icloud.com>
2026-04-12 18:28:52 +0200

    feat: simplify Home component by adding new FeaturedProjects and HeroHeader components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,62 +1,15 @@
-import profileImage from "../assets/images/profile.webp";
-import { Link } from "react-router-dom";
-import { projects } from "../data/projects.js";
-import { useState } from "react";
 import GetInTouch from "../components/GetInTouch.jsx";
-import CustomLink from "../components/CustomLink.jsx";
-import Card from "../components/card/Card.jsx";
-import GitHubIcon from "../assets/icons/github.svg?react";
+import FeaturedProjects from "../components/FeaturedProjects.jsx";
+import HeroHeader from "../components/HeroHeader.jsx";
 
 export default function Home() {
-  const [featuredProjects, _] = useState(
-    projects.filter((project) => project.featured === true),
-  );
   return (
     <>
       <section>
-        <img src={profileImage} alt="Jacob Asker" className="profile-image" />
-
-        <h2>Jacob Asker</h2>
-
-        <p>
-          I'm a Full-stack Developer with a bachelor's in Informatics,
-          specializing in JavaScript/TypeScript, Node.js, and React.
-        </p>
-
-        <small className="text-secondary">
-          Gothenburg, Sweden | Available for work
-        </small>
-        <div className="btn-group">
-          <CustomLink
-            href="mailto:jacob.asker@icloud.com"
-            className="button small hover-button-invert"
-          >
-            Contact me
-          </CustomLink>
-          <CustomLink
-            href="https://github.com/Jackan04?tab=repositories"
-            className="button small outline hover-button-fill"
-          >
-            <GitHubIcon className="icon-small" />
-            GitHub
-          </CustomLink>
-        </div>
+        <HeroHeader />
       </section>
-
       <section>
-        <div className="hstack justify-between">
-          <h2>Featured Projects</h2>
-          <Link to="/projects" className="hover-accent">
-            See all
-          </Link>
-        </div>
-        <ul className="vstack">
-          {featuredProjects.map((item) => (
-            <li key={item.url}>
-              <Card title={item.title} desc={item.preview} url={item.url} />
-            </li>
-          ))}
-        </ul>
+        <FeaturedProjects />
       </section>
 
       <section>

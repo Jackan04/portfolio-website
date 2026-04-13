b6ce67ba5a0db3967017bc0d396933b5fa774c3e
jackan04 <jacob.asker@icloud.com>
2026-04-13 10:06:25 +0200

    refactor: add back ChevronRightIcon in card and adjust layout

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,17 @@
 import styles from "./Card.module.css";
 import CustomLink from "../CustomLink";
+import ChevronRightIcon from "../../assets/icons/chevron-right.svg?react";
 
 export default function Card({ title, desc, url }) {
   return (
     <CustomLink href={url} className={styles.card}>
       <div className="hstack justify-between">
-        <b className={styles.title}>{title}</b>
+        <div>
+          <b className={styles.title}>{title}</b>
+          <p>{desc}</p>
+        </div>
+        <ChevronRightIcon className="icon-small" />
       </div>
-      <p>{desc}</p>
     </CustomLink>
   );
 }

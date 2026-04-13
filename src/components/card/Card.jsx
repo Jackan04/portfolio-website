db881227975266c4702f55dd4370db64bcdf4063
jackan04 <jacob.asker@icloud.com>
2026-04-13 10:45:00 +0200

    refactor: update Card component structure and styles for better layout

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,7 +5,7 @@ import ChevronRightIcon from "../../assets/icons/chevron-right.svg?react";
 export default function Card({ title, desc, url }) {
   return (
     <CustomLink href={url} className={styles.card}>
-      <div className="hstack justify-between">
+      <div className={styles.content}>
         <div>
           <b className={styles.title}>{title}</b>
           <p>{desc}</p>

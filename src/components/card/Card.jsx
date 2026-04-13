13633c1a4ba227aeb935f206f63074a97e73f72a
jackan04 <jacob.asker@icloud.com>
2026-04-13 08:53:18 +0200

    feat: remove ChevronRightIcon

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,11 @@
 import styles from "./Card.module.css";
 import CustomLink from "../CustomLink";
-import ChevronRightIcon from "../../assets/icons/chevron-right.svg?react";
 
 export default function Card({ title, desc, url }) {
   return (
     <CustomLink href={url} className={styles.card}>
       <div className="hstack justify-between">
         <b className={styles.title}>{title}</b>
-        <ChevronRightIcon className={`${styles.ChevronRightIcon} icon-small`} />
       </div>
       <small>{desc}</small>
     </CustomLink>

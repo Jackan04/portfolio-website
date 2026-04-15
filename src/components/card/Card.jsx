08af94437ef4ae3302c417fe96987bc5b73e3531
jackan04 <jacob.asker@icloud.com>
2026-04-15 21:10:21 +0100

    refactor: simplify Card component structure

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,13 +5,11 @@ import ChevronRightIcon from "../../assets/icons/chevron-right.svg?react";
 export default function Card({ title, desc, url }) {
   return (
     <CustomLink href={url} className={styles.card}>
-      <div className={styles.content}>
-        <div>
-          <b className={styles.title}>{title}</b>
-          <p>{desc}</p>
-        </div>
+      <div className="hstack gap-sm">
+        <b className={styles.title}>{title}</b>
         <ChevronRightIcon className="icon-small" />
       </div>
+      <p>{desc}</p>
     </CustomLink>
   );
 }
